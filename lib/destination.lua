29bc401a573f3261f9f77e0acdbf1b6205a48e16
notoj <1358519169@qq.com>
2019-09-01 00:56:35 +0800

    add:终点判定

--- lib/destination.lua
+++ lib/destination.lua
@@ -1,9 +1,23 @@
 Destination = Cuboid:extend()
 
-function Destination:new(x, y, z, nextScreen)
-    local len = 50
-    
+local len = 50
+
+function Destination:new(x, y, z, nextScreen) 
     Destination.super.new(self, x, y, z, len, len, len, {0.5, 1, 0.5})
     self.nextScreen = nextScreen
 end
 
+-- player touch
+function Destination:touch(player)
+    local flag = false
+    local centerX = self.x + len/2
+    local centerY = self.y + len/2
+    local disMin = len/2 + player.radius + 1
+    local disX = math.abs(player.x - centerX)
+    local disY = math.abs(player.y - centerY)
+    local dis = math.sqrt(math.pow(disX, 2)+math.pow(disY, 2))
+    if dis < disMin then
+        flag = true
+    end
+    return flag
+end
