36bee2851b5cf9b596dff3c3f100b24559620c3d
notoj <1358519169@qq.com>
2019-09-06 13:47:57 +0800

    主界面增加选关

--- screens/mainScreen.lua
+++ screens/mainScreen.lua
@@ -4,20 +4,44 @@ function Screen:new(ScreenManager)
 	self.screen = ScreenManager
 end
 
+local page
+
 function Screen:activate()
+	page = 1
 end
 
 function Screen:draw()
+	love.graphics.setColor(1,1,1)
+
 	-- credits
 	base.print("mofish", base.guiWidth/2, base.guiHeight/3, "center", "center")
 
 	--pressed start
-	base.print("按 A 开始", base.guiWidth/2, base.guiHeight/3 *2, "center", "center")
+	base.print("↑↓选关，A开始", base.guiWidth/2, base.guiHeight/3 *2, "center", "center")
+	-- level choice
+	base.print("\n关卡选择：" .. page, base.guiWidth/2, base.guiHeight/3 *2, "center", "center")
 end
 
 function Screen:keypressed(key)
+	-- start level
 	if key == keys.A then
-		self.screen:view(levelString[1])
+		self.screen:view(levelString[page])
+	end
+
+	-- choice level
+	local levelMax = #levelString-1--the last one is finishScreen
+	if key == keys.DPad_down then
+		if page < levelMax then
+			page = page + 1
+		else
+			page = 1
+		end
+	elseif key == keys.DPad_up then
+		if page > 1 then
+			page = page - 1
+		else
+			page = levelMax
+		end
 	end
 end
 
